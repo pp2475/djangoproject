941c85285d48edc4a1d3534192b131d12f9a446b
pp2475 <17117561@qq.com>
2018-12-16 21:55:59 +0800

    Django blog homepage

--- blog/views.py
+++ blog/views.py
@@ -3,9 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import HttpResponse
+from .models import Post
 
 def index(request):
+    post_list = Post.objects.all().order_by('-created_time')
     return render(request, 'blog/index.html', context={
-        'title':'我的博客首页',
-        'welcome':'欢迎访问我的博客首页'
+        'post_list': post_list
     })
