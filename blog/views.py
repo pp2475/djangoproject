dd9f1be55b1d3ea13a047010525281910344eaca
pp2475 <17117561@qq.com>
2018-12-16 20:57:02 +0800

    blog index view

--- blog/views.py
+++ blog/views.py
@@ -5,4 +5,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('Welcome to my blog')
+    return render(request, 'blog/index.html', context={
+        'title':'我的博客首页',
+        'welcome':'欢迎访问我的博客首页'
+    })
