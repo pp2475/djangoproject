858b8d4ab487b29112ae5cfb6c7a134d0c7c7a5b
pp2475 <17117561@qq.com>
2018-12-22 14:49:28 +0800

    archives and categories

--- blog/views.py
+++ blog/views.py
@@ -4,13 +4,24 @@ from django.shortcuts import render, get_object_or_404
 # Create your views here.
 
 from django.http import HttpResponse
-from .models import Post
+from .models import Post, Category
 
 def index(request):
     post_list = Post.objects.all().order_by('-created_time')
     return render(request, 'blog/index.html', context={
         'post_list': post_list
     })
+
+def category(request, pk):
+    cate = get_object_or_404(Category, pk=pk)
+    post_list=Post.objects.filter(category=cate).order_by('-created_time')
+    return render(request, 'blog/index.html', context={'post_list':post_list})
+
+def archives(request, year, month):
+    post_list = Post.objects.filter(created_time__year=year,
+                                    created_time__month=month,
+                                    ).order_by('-created_time')
+    return render(request, 'blog/index.html', context={'post_list':post_list})
 
 def detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
